3aa78bdf13d20f9835d593715c93931d5e1f4ad7
Denis <denis.bortnikov17@gmail.com>
2022-10-08 14:34:24 +0300

    remove console.log

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import { Jobs } from "components/Jobs";
@@ -8,10 +7,6 @@ import { Employees } from "components/Employees";
 const drawerWidth = 240;
 
 export const App = () => {
-  const store = useSelector((state) => state);
-
-  console.log("store", store);
-
   return (
     <Box sx={{ display: "flex" }}>
       <Jobs />
